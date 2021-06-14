6e5e8be5552a738d7a596df511193dc2d91b1bb4
NandyPriya <85861199+NandyPriya@users.noreply.github.com>
2021-06-14 11:14:02 +0530

    detect both face mask and social distance file

---
+++ video.py
@@ -0,0 +1,173 @@
+
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import math
+
+arg = argparse.ArgumentParser()
+arg.add_argument("--video", required=True, help="path to input video")
+arg.add_argument("-d", "--distance", type=float, default=100.0, help="pixels distance to calculate social distance")
+arg.add_argument("--frames", type=int, default=20, help="number of frames to skip while testing video")
+args = vars(arg.parse_args())
+
+labelsPath = "coco.names"
+LABELS = open(labelsPath).read().strip().split("\n")
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+                           dtype="uint8")
+weightsPath = "yolo-coco/yolov3.weights"
+configPath = "yolo-coco/yolov3.cfg"
+
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+confidence_threshold = 0.4
+
+print("[INFO] loading face detector model...")
+prototxtPath = "face_detector/deploy.prototxt"
+weightsPath = "face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+model_store_dir= "classifier.model"
+maskNet = load_model(model_store_dir)
+
+cap = cv2.VideoCapture(args["video"])
+index = 0
+cnt = 0
+while (cap.isOpened()):
+  ret, image = cap.read()
+
+  if ret == False:
+      break
+  index += 1
+  if index%args["frames"] == 0:
+    image = cv2.resize(image, (640, 360))
+    (H, W) = image.shape[:2]
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+    print("Time taken to predict the image: {:.6f}seconds".format(end-start))
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > 0.1 and classID == 0:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+    ind = []
+    for i in range(0, len(classIDs)):
+        if (classIDs[i] == 0):
+            ind.append(i)
+
+    a = []
+    b = []
+
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            a.append(x)
+            b.append(y)
+   
+
+    distance = []
+    nsd = []
+    for i in range(0, len(a) - 1):
+        for k in range(1, len(a)):
+            if (k == i):
+                break
+            else:
+                x_dist = (a[k] - a[i])
+                y_dist = (b[k] - b[i])
+                d = math.sqrt(x_dist * x_dist + y_dist * y_dist)
+                distance.append(d)
+                if (d <= args["distance"]):
+                    nsd.append(i)
+                    nsd.append(k)
+                nsd = list(dict.fromkeys(nsd))
+
+    color = (0, 0, 255)
+    for i in nsd:
+        (x, y) = (boxes[i][0], boxes[i][1])
+        (w, h) = (boxes[i][2], boxes[i][3])
+        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+        text = "Alert"
+        cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    color = (0,255,0)
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            if (i in nsd):
+                break
+            else:
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+                text = 'OK'
+                cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 1.0, (416, 416), (104.0, 177.0, 123.0))
+
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    for i in range(0, detections.shape[2]):
+        confidence =detections[0, 0, i, 2]
+
+        if confidence > confidence_threshold:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            face = image[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+
+            (mask, without_mask) = maskNet.predict(face)[0]
+            label = "Mask" if mask > without_mask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+            label = "{}: {:.2f}%".format(label, max(mask, without_mask) * 100)
+            cv2.putText(image, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+            print("End of classifier")
+
+    im = cv2.resize(image, (800, 512))
+    cv2.imshow("Image", im)
+    cv2.imwrite('result_frames/'+str(cnt)+'.png', image)
+    cnt = cnt + 1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
